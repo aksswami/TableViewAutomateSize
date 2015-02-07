a82ddf88c787a875b707863361c1bf6a79e03768
Amit Kumar Swami <amitiitm2009@gmail.com>
2015-02-08 00:30:57 +0530

    Removed unwanted code.

--- TableViewAutomaticSize/ViewController.m
+++ TableViewAutomaticSize/ViewController.m
@@ -10,12 +10,6 @@
 #import "CustomTableViewCell.h"
 #include <stdlib.h>
 
-#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
-#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
-#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
-#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
-#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
-
 @interface ViewController ()
 
 @property (nonatomic, strong) NSString * str;
